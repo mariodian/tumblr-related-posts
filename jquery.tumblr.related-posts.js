ef921dd1fcba0b7d6445dee308aa09fed0211b76
Mario Dian <mariodian@gmail.com>
2014-06-03 13:45:32 +0200

    Addes merge sort because browsers implement different sort.

--- jquery.tumblr.related-posts.js
+++ jquery.tumblr.related-posts.js
@@ -1,3 +1,100 @@
+/*!
+ * Merge Sort in JavaScript v1.0
+ * http://github.com/justinforce/merge-sort
+ *
+ * Copyright (c) 2011, Justin Force
+ * Licensed under the BSD 3-Clause License
+ */
+
+/*jslint browser: true, indent: 2 */
+/*global jQuery */
+(function () {
+	'use strict';
+	
+	// Add stable merge sort method to Array prototype
+	if( !Array.mergeSort )
+	{
+		Array.prototype.mergeSort = function(compare){
+			var length = this.length,
+				middle = Math.floor(length / 2);
+	
+			// define default comparison function if none is defined
+			if( !compare ) 
+			{
+				compare = function(left, right){
+					if( left  <  right )
+					{
+						return -1;
+					} 
+					else if( left === right )
+					{
+						return 0;
+					} 
+					else 
+					{
+						return 1;
+					}
+				}
+			}
+	
+			if( length < 2 ) 
+			{
+				return this;
+			}
+	
+			function merge(left, right, compare){
+				var result = [];
+	
+				while( left.length > 0 || right.length > 0 )
+				{
+					if( left.length > 0 && right.length > 0 )
+					{
+						if( compare(left[0], right[0]) <= 0 )
+						{
+							result.push(left[0]);
+							
+							left = left.slice(1);
+						}
+						else
+						{
+							result.push(right[0]);
+							
+							right = right.slice(1);
+						}
+					}
+					else if( left.length > 0 )
+					{
+						result.push(left[0]);
+						
+						left = left.slice(1);
+					}
+					else if( right.length > 0 )
+					{
+						result.push(right[0]);
+						
+						right = right.slice(1);
+					}
+				}
+				
+				return result;
+			}
+	
+			return merge(this.slice(0, middle).mergeSort(compare), this.slice(middle, length).mergeSort(compare), compare);
+		};
+	}
+	
+	// Add merge sort to jQuery if it's present
+	if( window.jQuery !== undefined ) 
+	{
+		jQuery.fn.mergeSort = function(compare){
+			return jQuery(Array.prototype.mergeSort.call(this, compare));
+		};
+		jQuery.mergeSort = function(array, compare){
+			return Array.prototype.mergeSort.call(array, compare);
+		};
+	}
+}());
+
 /**
 *	Create list of related posts by tag
 **/
@@ -24,7 +121,7 @@ $.fn.relatedPosts = function(options){
 	*	Remove duplicate objects by key
 	**/
 	var removeDuplicatesByKey = function(list, key){
-		list.sort(function(a, b){ 
+		list.sort(function(a, b){
 			return a[key] - b[key];
 		});
 		
@@ -54,14 +151,16 @@ $.fn.relatedPosts = function(options){
 
 		// Sort by ID first
 		postsList.sort(function(a, b){
-			return a.id < b.id;
+			return a.id < b.id ? 1 : -1;
 		});
-			
+		
+		var tempList;
+
 		// Then sort by a tag priority
 		for( var i = (tags.length - 1); i >= 0; i-- )
 		{		
-			postsList.sort(function(a, b){
-				if( a.tags[i] == tags[i])
+			tempList = postsList.mergeSort(function(a, b){
+				if( a.tags[i] == tags[i] || b.tags[i] == a.tags[i] )
 				{
 					return 1;
 				}
@@ -73,9 +172,9 @@ $.fn.relatedPosts = function(options){
 		}
 
 		// Reverse the list
-		postsList.reverse();
+		tempList.reverse();
 		
-		var postsCount = postsList.length;
+		var postsCount = tempList.length;
 		
 		if( postsCount )
 		{
@@ -87,7 +186,7 @@ $.fn.relatedPosts = function(options){
 			
 			for( var i = 0; i < (postsCount > settings.maxPosts ? settings.maxPosts : postsCount); i++ )
 			{
-				var post = postsList[i],
+				var post = tempList[i],
 					image = null,
 					imageTag = '',
 					cleanBody = '',
